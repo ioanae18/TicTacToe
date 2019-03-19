f6f2d14ed3576ee9e3de16b4b08b5bbcc305270a
Emilia Ioana <emmiii_xd@windowslive.com>
2019-03-19 23:51:51 +0200

    Tic Tac Toe Game

---
+++ TicTacToe/MarkType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+	///<summary>
+	///The type of value a cell is curently at
+	///</summary>
+
+	public enum MarkType
+	{
+		/// <summary>
+		/// The cell hasn't been clicked yet
+		/// </summary>
+		Free,
+		/// <summary>
+		/// The cell is 0
+		/// </summary>
+		Nought,
+		/// <summary>
+		/// The cell is X
+		/// </summary>
+		Cross
+	}
+}
